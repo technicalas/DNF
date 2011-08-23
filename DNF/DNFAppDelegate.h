811b499b99d4375085c0fa02bea4700a3cae54f5
Daniel Rueda Jimenez <rujida@gmail.com>
2011-08-22 19:45:01 -0500

    Initial Commit

---
+++ DNF/DNFAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DNFAppDelegate.h
+//  DNF
+//
+//  Created by Daniel Rueda Jimenez on 22/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNFAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
---
+++ DNF/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  DNF
+//
+//  Created by Daniel Rueda Jimenez on 22/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
